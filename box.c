5abc5789ccc8c676886697174fc20dc981ba2731
Varun17g <31441145+Varun17g@users.noreply.github.com>
2018-08-30 19:05:00 +0530

    Square box inside square box

---
+++ box.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=7;j++)
+		{
+			if(j==1||j==7||i==1||i==7||(j>=3&&j<=5&&(i==3||i==5))||(j==3||j==5)&&i==4)
+				printf("* ");
+			else
+				printf("  ");
+		}
+		printf("\n");
+	}
+}
