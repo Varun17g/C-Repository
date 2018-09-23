9904a6b0562fa6316c23fc51d6beb8d4681a3297
Varun Bhati <varunbhati17@gmail.com>
2018-09-23 15:53:42 +0530

    Remove all occurence of character from given sentence by help of dma and functions

---
+++ exercise14.c
@@ -0,0 +1,36 @@
+//Remove all occurence of character from given sentence by help of dma and functions
+#include<stdio.h>
+#include<conio.h>
+int length(char * string)
+{
+	int i;
+	for(i=0;*(i+string)!=NULL;i++);
+	return i;
+}
+void removes(char * string,char character)
+{
+	int i,j,count;
+	count=length(string)-1;
+	for(i=count;i>=0;i--)
+	{
+		if(*(i+string)==character)
+		{
+			for(j=i;*(j+string)!=NULL;j++)
+			{
+				*(string+j)=*(string+j+1);
+			}
+		}
+	}
+}
+void main()
+{
+	char *string,character;
+	string=(char*)malloc(sizeof(char));
+	printf("Enter string\n");
+	gets(string);
+	printf("Enter character to remove all occurence of it\n");
+	scanf("%c",&character);
+	removes(string,character);
+	puts(string);
+	free(string);
+}
